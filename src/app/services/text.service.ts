8e1a3118ead7c6b07c94620e9c8eb7a9d7b3a9e2
Marcelo Aguilera <marceloaad@gmail.com>
2020-06-28 21:37:23 +0200

    Wrapper Style Fixes

--- src/app/services/text.service.ts
+++ src/app/services/text.service.ts
@@ -14,7 +14,8 @@ export class TextService {
   getTexts() {
     return [
       {text: 'Steve Jobs: He Brought the Show to Business', source: 'The New York Times', doc_id: 0},
-      {text: 'Silicon Valley\'s Youth Problem', source: 'The New York Times', doc_id: 1}
+      {text: 'Silicon Valley\'s Youth Problem', source: 'The New York Times', doc_id: 1},
+      {text: 'COVID-19 Response in Sub-Saharan Low-Resource Setting: Healthcare Soldiers Need Bullets', source: 'The American Society of Tropical Medicine and Hygiene', doc_id: 2},
     ];
   }
 }
